873ce10d5e1c2d57698a50d2cda2d9ea8f6f0c84
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-28 14:20:14 -0400

    started to put in basic html for front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <>
+        <div class="container" id="main-content-container">
+          <div class="row" id="main-content-row">
+
+            <h1>Piano Roll</h1>
+
+            <div class="col-12" id="main-content-column">
+
+
+
+              <div class="form-group row">
+                <label for="staticEmail" class="col-sm-2 col-form-label">Input a URL</label>
+                <div class="col-sm-10">
+                  <input type="text" readonly class="form-control-plaintext" id="staticURL" value="" placeholder="https://www.google.com" />
+                </div>
+              </div>
+
+
+
+
+
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </>
     );
   }
 }
 
 export default App;
+
