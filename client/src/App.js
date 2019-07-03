cf6edbb5316ec68b7940603c892f63e81c4d661c
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-02 20:20:09 -0400

    got small synth example working

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,58 @@
 import React, { Component } from "react";
 import "./App.css";
-
-const MonoSynth = require("Tone").MonoSynth;
-
-//create a synth and connect it to the master output (your speakers)
-const synth = new MonoSynth().toMaster();;
-
-
-
-
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease('C4', '8n');
+const Tone = require("tone");
 
 class App extends Component {
 
+  state = {
+    audioContext: {}
+  }
+
+  componentDidMount() {
+
+    const context = new AudioContext();
+
+    this.setState({ audioContext: context });
+  }
+
+  playSynth = () => {
 
 
+
+    const synth = new Tone.FMSynth().toMaster();
+    //schedule a series of notes to play as soon as the page loads
+    synth.triggerAttackRelease('C4', '4n', '8n')
+    synth.triggerAttackRelease('E4', '8n', Tone.Time('4n') + Tone.Time('8n'))
+    synth.triggerAttackRelease('G4', '16n', '2n')
+    synth.triggerAttackRelease('B4', '16n', Tone.Time('2n') + Tone.Time('8t'))
+    synth.triggerAttackRelease('G4', '16', Tone.Time('2n') + Tone.Time('8t') * 2)
+    synth.triggerAttackRelease('E4', '2n', '0:3')
+
+    this.state.audioContext.resume().then(() => {
+      console.log('Playback resumed successfully');
+    });
+
+
+  }
 
 
   render() {
     return (
       <>
-        <div class="container" id="main-content-container">
-          <div class="row" id="main-content-row">
+        <div className="container" id="main-content-container">
+          <div className="row" id="main-content-row">
 
             <h1>Piano Roll</h1>
 
-            <div class="col-12" id="main-content-column">
+            <div className="col-12" id="main-content-column">
 
 
 
-              <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Input a URL</label>
-                <div class="col-sm-10">
-                  <input type="text" readonly class="form-control-plaintext" id="staticURL" value="" placeholder="https://www.google.com" />
-                  <button type="submit" onClick={() => { alert("click");}} class="btn btn-primary mb-2">Submit</button>
+              <div className="form-group row">
+                <label className="col-sm-2 col-form-label">Input a URL</label>
+                <div className="col-sm-10">
+                  <input type="text" readOnly className="form-control-plaintext" id="staticURL" value="" placeholder="https://www.google.com" />
+                  <button type="submit" onClick={this.playSynth} className="btn btn-primary mb-2">Submit</button>
                 </div>
               </div>
 
