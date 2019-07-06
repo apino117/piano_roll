7e55c2b27cff20aeb0a10dce26fdf694bec1bb08
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-06 11:59:43 -0400

    tried to install scraper from activity

--- server.js
+++ server.js
@@ -2,6 +2,62 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Require all models
+const db = require("./models");
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/pianoRollDB", { useNewUrlParser: true });
+
+app.get("/scrape", function (req, res) {
+
+  axios.get("http://www.echojs.com/").then(function (response) {
+
+    const $ = cheerio.load(response.data);
+
+    $("article h2").each(function (i, element) {
+
+      const result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.url = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Website.create(result)
+        .then(function (dbWebsite) {
+          console.log(dbWebsite);
+        })
+        .catch(function (err) {
+          console.log(err)
+        });
+    })
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,10 +66,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
