bd30c5e3f2f7f1344999deecccc3e63c0f92638c
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-06 11:31:06 -0400

    adjusting Website data model

--- models/Website.js
+++ models/Website.js
@@ -6,21 +6,26 @@ var Schema = mongoose.Schema;
 
 var websiteSchema = new Schema({
 
-    // url, a string, must be entered
+    title: {
+      type: String,
+      required: true
+    },
     url: {
       type: String,
       required: true,
-      unique: { index: { unique: true } }
+      // unique: { index: { unique: true } }
     },
-    // date is just a string
     date: {
       type: Date,
       default: Date.now
     },
-    saved: {
-      type: Boolean,
-      default: false
+    midi: {
+      type: String
     }
+    // saved: {
+    //   type: Boolean,
+    //   default: false
+    // }
   });
 
   // Create the Headline model using the headlineSchema
