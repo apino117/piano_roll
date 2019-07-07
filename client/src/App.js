954c040c53d15fa6ea143f96a85a41af7d7e6ef8
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-07 18:24:27 -0400

    added profile page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 const Tone = require("tone");
 
@@ -84,6 +85,7 @@ class App extends Component {
               <Route component={NoMatch} /> */}
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
             </Switch>
           </div>
         </Router>
