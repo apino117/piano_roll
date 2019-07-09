84898939bdb48c52eb9c23445a5075736bbd52ad
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-09 18:48:50 -0400

    deleted final roll reference

--- routes/api/roll.js
+++
@@ -1,6 +0,0 @@
-// const router = require("express").Router();
-// const rollController = require("../../controllers/roll");
-
-// router.get("/", rollController.rollWebsite);
-
-// module.exports = router;
