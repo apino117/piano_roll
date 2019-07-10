da2a01b13ede006a4f205a8628ddadcabcaaeab2
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-09 20:27:19 -0400

    got charcode conversipn

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,34 +3,39 @@ const cheerio = require("cheerio");
 
 const scrape = function () {
 
-    return axios.get("http://www.nytimes.com/").then(function (response) {
+    return axios.get("https://jameshfisher.com/2018/11/25/google-ngram-api/").then(function (response) {
 
         const $ = cheerio.load(response.data);
 
         console.log("scraping");
 
 
-        let websites = []; // create blank arrays to hold our final website and to hold the tag data we're gonna push
+        // let websites = []; // create blank arrays to hold our final website and to hold the tag data we're gonna push
 
         let dataToAdd = {
             title: $("title").text(), // title we can set already cause it's always the same HTML
-            tags: []
+            tags: [],
         }
 
-        
+
         $('body').children().each(function (index, element) { // for every child of the main body
 
             let tags = (element.tagName); // grab the tagname
 
-            dataToAdd.tags.push(tags); // push that to the tags section of the array
+            for (let i = 0; i < tags.length; i++) {
+            //    console.log(tags[i]);
+               dataToAdd.tags.push(tags[i].charCodeAt(0)); // push that to the tags section of the array
+            }
 
         });
 
         console.log(dataToAdd); // just the see what we're working with
 
-        websites.push(dataToAdd); // push to the websites array 
 
-        return websites;
+
+        // websites.push(dataToAdd); // push to the websites array 
+
+        return dataToAdd;
     });
 }
 
