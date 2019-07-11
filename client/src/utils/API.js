9c37f1908f65ec09d504f4b3934ff7511e75ef92
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-11 17:50:07 -0400

    changed get to post and put in scrapedata function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,26 @@ import axios from "axios";
 
 export default {
 
-scrapeWebsite: (q) => {
-    return axios.get("/api/fetch", {params: {q}})
-},
+    scrapeWebsite: (q) => {
+        return axios.post("/api/fetch", { params: { q } })
+    },
 
-alert: (thingToSay) => {
-    alert(thingToSay)
-}
+    alert: (thingToSay) => {
+        alert(thingToSay)
+    },
+
+    scrapeData: function(websiteData) {
+        return axios.get(websiteData)
+    },
+
+    // scrapeData:(websiteData)
+    // {axios.get(wb)}
+    // return 
+
+    saveWebsite: function (websiteData) {
+        console.log("inside savewebsite "+ websiteData);
+        return axios.post("/api/fetch", websiteData)
+    },
 
 
 }
