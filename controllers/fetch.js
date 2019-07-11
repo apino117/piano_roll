d666b68bf096df406a166054eec2d764c26b8691
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-11 17:51:15 -0400

    made create function in fetch controller

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -6,11 +6,14 @@ let useThisUrl = "http://reductress.com/"
 module.exports = {
 
     scrapedWebsites: function (req, res) {
-        
+
+        console.log("this is the scrapedWebsites jaunt");
+        console.log(req.params);
         // const {query: params} = useThisUrl;
 
         return scrape(useThisUrl)
             .then(function (websites) {
+                console.log("websites " + websites)
                 return db.Website.create(websites);
             })
             .then(function () {
@@ -23,5 +26,13 @@ module.exports = {
                     message: "this is an error " + err
                 })
             })
-    }
-}
+    },
+    create: function (req, res) {
+
+        //expected data should look like {  url: "sgafafa", name:"fasfafa"}
+        console.log("inside fetch controller passed data:")
+        console.log(req.body);
+       db.Website   
+       .create(req.body)     
+       .then(dbModel => res.json(dbModel))     
+       .catch(err => res.status(422).json(err));  }, }
