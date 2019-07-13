173ceb832b006c63a850a121138ebfc48dfd5cf8
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-12 20:28:20 -0400

    created storeurl fucntion

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 export default {
 
+    storeUrl: (nameOfWebsite) => {
+        console.log(`this is the store url function and name of website: ${nameOfWebsite}`);
+        return axios.post("api/url", nameOfWebsite);
+    },
+
     // scrapeWebsite: (q) => {
     //     return axios.post("/api/fetch", { params: { q } })
     // },
