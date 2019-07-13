92d3576e1e399c26f297bd4164c8ffdbd955a608
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-13 12:38:42 -0400

    put in a route to get by id

--- routes/api/url.js
+++ routes/api/url.js
@@ -4,7 +4,12 @@ const urlController = require("../../controllers/urlController");
 // Matches with /api/url
 router.route("/")
     .get(urlController.findAll)
-    .post(urlController.create)
+    .post(urlController.create);
+
+// Matches with /api/url/:id
+router.route("/:id")
+    .get(urlController.findById);
+
 
 
 module.exports = router;
