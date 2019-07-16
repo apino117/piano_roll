9b94609d83ddf11a5ecee701b179eff1d803c392
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-16 18:45:16 -0400

    removed app.get(*)

--- server.js
+++ server.js
@@ -69,11 +69,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
