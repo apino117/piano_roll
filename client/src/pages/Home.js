75a1e1ef04cf05cd2b3898c338e56e47edecdd38
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-07-17 20:34:48 -0400

    formatting, moving h2 in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,211 +9,215 @@ const Tone = require("tone");
 
 class Home extends Component {
 
-  state = {
-    objectForNotes: {},
-    audioContext: {},
-    search: "",
-    titles: [],
-    results: [],
-    q: "",
-    error: "",
-    loadMessage: "",
-    searchMessage: "",
-  }
+    state = {
+        objectForNotes: {},
+        audioContext: {},
+        search: "",
+        titles: [],
+        results: [],
+        q: "",
+        error: "",
+        loadMessage: "",
+        searchMessage: "",
+    }
 
-  // ==================================================|||===================================================== //
-  // ======================== *********** ||| GENERAL REACT FUNCTIONS || *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
+    // ==================================================|||===================================================== //
+    // ======================== *********** ||| GENERAL REACT FUNCTIONS || *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
 
-  componentDidMount = () => {
+    componentDidMount = () => {
 
-    this.runTheList();
-
-    const context = new AudioContext();
-
-    this.setState({
-      audioContext: context,
-      loadMessage: "Load Synth"
-    });
-  };
-
-  handleInputChange = event => {
-
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-
-  };
-
-  // ==================================================|||===================================================== //
-  // ======================== *********** ||||| FORM / API FUNCTIONS ||| *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-
-  runTheList = () => {
-    API.getUrlsList()
-      .then(res => this.setState({ titles: this.pullTitlesFromResults(res.data) }))
-      .then(() => console.log(this.state.titles))
-      .catch(err => console.log(err));
-  }
-
-  pullTitlesFromResults = (array) => {
-
-    let justTitles = [];
-    for (let i = 0; i < array.length; i++) {
-      justTitles.push(array[i].title)
-    };
-    return justTitles;
-  }
-
-  handleSearchInput = event => {
-    this.setState({ search: event.target.value });
-    console.log(this.state.search)
-  }
-
-  handleUrlSubmit = event => {
-    event.preventDefault();
-
-    const urlToScrape = {
-      url: this.state.q
-    };
-
-    API.storeUrl(urlToScrape)
-      .then(() => API.getUrl(urlToScrape.url))
-      .then(() => {
         this.runTheList();
-      })
-    this.setState({
-      q: " ",
-    });
-  };
 
-  handleTitleSubmit = event => {
-    event.preventDefault();
-    API.getNoteObjectByTitle(this.state.search)
-      .then(res => {
-        console.log("this is the result data", res.data)
+        const context = new AudioContext();
+
         this.setState({
-          objectForNotes: res.data
+            audioContext: context,
+            loadMessage: "Load Synth"
+        });
+    };
+
+    handleInputChange = event => {
+
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+
+    };
+
+    // ==================================================|||===================================================== //
+    // ======================== *********** ||||| FORM / API FUNCTIONS ||| *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+
+    runTheList = () => {
+        API.getUrlsList()
+            .then(res => this.setState({ titles: this.pullTitlesFromResults(res.data) }))
+            .then(() => console.log(this.state.titles))
+            .catch(err => console.log(err));
+    }
+
+    pullTitlesFromResults = (array) => {
+
+        let justTitles = [];
+        for (let i = 0; i < array.length; i++) {
+            justTitles.push(array[i].title)
+        };
+        return justTitles;
+    }
+
+    handleSearchInput = event => {
+        this.setState({ search: event.target.value });
+        console.log(this.state.search)
+    }
+
+    handleUrlSubmit = event => {
+        event.preventDefault();
+
+        const urlToScrape = {
+            url: this.state.q
+        };
+
+        API.storeUrl(urlToScrape)
+            .then(() => API.getUrl(urlToScrape.url))
+            .then(() => {
+                this.runTheList();
+            })
+        this.setState({
+            q: " ",
+        });
+    };
+
+    handleTitleSubmit = event => {
+        event.preventDefault();
+        API.getNoteObjectByTitle(this.state.search)
+            .then(res => {
+                console.log("this is the result data", res.data)
+                this.setState({
+                    objectForNotes: res.data
+                })
+            })
+            .then(() => {
+                console.log("this is the objectForNotes", this.state.objectForNotes)
+            })
+        this.setState({
+            loadMessage: "Synth Loaded",
+            searchMessage: this.state.search
         })
-      })
-      .then(() => {
-        console.log("this is the objectForNotes", this.state.objectForNotes)
-      })
-    this.setState({
-      loadMessage: "Synth Loaded",
-      searchMessage: this.state.search
-    })
-  };
+    };
 
-  // ==================================================|||===================================================== //
-  // ======================== *********** |||||| MUSIC FUNCTIONS ||||||| *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
+    // ==================================================|||===================================================== //
+    // ======================== *********** |||||| MUSIC FUNCTIONS ||||||| *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
 
-  mapToStandard = (number) => {
-    let numberToReturn = number;
-    while (numberToReturn > 127) {
-      numberToReturn -= 127;
-      // console.log(numberToReturn);
+    mapToStandard = (number) => {
+        let numberToReturn = number;
+        while (numberToReturn > 127) {
+            numberToReturn -= 127;
+            // console.log(numberToReturn);
+        }
+        return numberToReturn
+    };
+
+    getNoteObject = () => {
+
+        let noteObj = {};
+        let count = 0;
+        let column = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+        let columnCount = 0;
+
+        for (let i = -2; i < 9; i++) {
+            for (columnCount = 0; columnCount < 12; columnCount++) {
+                noteObj[count] = column[columnCount] + i;
+                count++;
+            }
+            columnCount = 0;
+        }
+        return noteObj;
+    };
+
+    schedulePlay = (note, length, time, synth) => {
+        const triggerFunc = (triggerTime) => {
+            synth.triggerAttackRelease(note, length, triggerTime)
+        }
+        Tone.Transport.schedule(triggerFunc, time)
     }
-    return numberToReturn
-  };
 
-  getNoteObject = () => {
-
-    let noteObj = {};
-    let count = 0;
-    let column = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-    let columnCount = 0;
-
-    for (let i = -2; i < 9; i++) {
-      for (columnCount = 0; columnCount < 12; columnCount++) {
-        noteObj[count] = column[columnCount] + i;
-        count++;
-      }
-      columnCount = 0;
+    playSynth = () => {
+        const synth = new Tone.FMSynth().toMaster();
+        this.state.objectForNotes.tags.forEach((tag, index) => {
+            this.schedulePlay(this.getNoteObject()[tag], '16n', index, synth)
+        })
+        synth.harmonicity.value = 0.6;
+        synth.detune.value = -1200;
+        synth.modulationIndex.value = 20;
+        synth.oscillator.type = "sine";
+        synth.envelope.attack = 0.01;
+        synth.envelope.decay = 0.01;
+        synth.envelope.sustain = 1;
+        synth.envelope.release = 0.08;
+        synth.modulation.type = "square";
+        synth.modulationEnvelope.attack = 0.07;
+        synth.modulationEnvelope.decay = 1;
+        synth.modulationEnvelope.sustain = 1;
+        synth.modulationEnvelope.release = 1;
+        synth.volume.value = -12;
+        Tone.Transport.toggle();
+        Tone.Transport.bpm.value = 250; //nice to have this be adjustible by the user..
+        this.state.audioContext.resume().then(() => {
+            console.log('Playback resumed successfully');
+        });
     }
-    return noteObj;
-  };
 
-  schedulePlay = (note, length, time, synth) => {
-    const triggerFunc = (triggerTime) => {
-      synth.triggerAttackRelease(note, length, triggerTime)
+
+    // ==================================================|||===================================================== //
+    // ======================== *********** ||||||||| JSK RENDER ||||||||| *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+    // ======================== ***********                                *********** ======================== //
+
+    render() {
+        return (
+            <>
+
+                <Alert type="danger" style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}>{this.state.error}</Alert>
+                <SearchForm
+                    handleInputChange={this.handleSearchInput}
+                    handleTitleSubmit={this.handleTitleSubmit}
+                    titles={this.state.titles}
+                ></SearchForm>
+                <Button type="submit" onClick={this.handleTitleSubmit} className="btn btn-success">
+                    {this.state.loadMessage}
+                </Button>
+
+                {/* ---------------------------------------------------------------------------------------------------------- */}
+                {/* ========================================================================================================== */}
+                {/* ---------------------------------------------------------------------------------------------------------- */}
+
+
+                <div className="container" id="main-content-container">
+                    <div className="row" id="main-content-row">
+                        <h1>HTML Piano Roll</h1>
+                        <div className="col-12" id="main-content-column">
+                            <h2>Hear your favorite websites!</h2>
+                            <button type="submit" onClick={this.playSynth} className="btn btn-primary mb-2">Play Synth: {this.state.searchMessage}</button>
+                            <Form
+                                handleInputChange={this.handleInputChange}
+                                handleUrlSubmit={this.handleUrlSubmit}
+                                q={this.state.q}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
     }
-    Tone.Transport.schedule(triggerFunc, time)
-  }
-
-  playSynth = () => {
-    const synth = new Tone.FMSynth().toMaster();
-    this.state.objectForNotes.tags.forEach((tag, index) => {
-      this.schedulePlay(this.getNoteObject()[tag], '16n', index, synth)
-    })
-    synth.harmonicity.value = 0.6;
-    synth.detune.value = -1200;
-    synth.modulationIndex.value = 20;
-    synth.oscillator.type = "sine";
-    synth.envelope.attack = 0.01;
-    synth.envelope.decay = 0.01;
-    synth.envelope.sustain = 1;
-    synth.envelope.release = 0.08;
-    synth.modulation.type = "square";
-    synth.modulationEnvelope.attack = 0.07;
-    synth.modulationEnvelope.decay = 1;
-    synth.modulationEnvelope.sustain = 1;
-    synth.modulationEnvelope.release = 1;
-    synth.volume.value = -12;
-    Tone.Transport.toggle();
-    Tone.Transport.bpm.value = 250; //nice to have this be adjustible by the user..
-    this.state.audioContext.resume().then(() => {
-      console.log('Playback resumed successfully');
-    });
-  }
-
-
-  // ==================================================|||===================================================== //
-  // ======================== *********** ||||||||| JSK RENDER ||||||||| *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-  // ======================== ***********                                *********** ======================== //
-
-  render() {
-    return (
-      <>
-
-        <Alert type="danger" style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}>{this.state.error}</Alert>
-        <SearchForm
-          handleInputChange={this.handleSearchInput}
-          handleTitleSubmit={this.handleTitleSubmit}
-          titles={this.state.titles}
-        ></SearchForm>
-        <Button type="submit" onClick={this.handleTitleSubmit} className="btn btn-success">
-          {this.state.loadMessage}
-        </Button>
-
-        {/* ========================================================================= */}
-        <div className="container" id="main-content-container">
-          <div className="row" id="main-content-row">
-            <h1>HTML Piano Roll</h1>
-            <h2>Hear your favorite websites!</h2>
-            <div className="col-12" id="main-content-column">
-              <button type="submit" onClick={this.playSynth} className="btn btn-primary mb-2">Play Synth: {this.state.searchMessage}</button>
-              <Form
-                handleInputChange={this.handleInputChange}
-                handleUrlSubmit={this.handleUrlSubmit}
-                q={this.state.q}
-              />
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
 }
 
 export default Home;
